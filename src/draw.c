3d792662f14cba8e1260b848477048c226943962
Haymi19 <haimanoteshete685@gmail.com>
2023-06-29 14:07:54 +0000

    Maze project

--- src/draw.c
+++ src/draw.c
@@ -1,65 +1,55 @@
-#include "../inc/main.h"
+#include "../inc/header.h"
 
 /**
- * draw_something - A basic function to draw a line
- *
- * @instance: An SDL instance of type struct SDL_Instance
+ * drawRect - draw a rectangle
+ * @x: x coordinate
+ * @y: y coordinate
+ * @width: rectangle width
+ * @height: rectangle height
+ * @color: pixel color
  */
-void draw_something(SDL_Instance *instance)
+
+void drawRect(int x, int y, int width, int height, color_t color)
 {
-	SDL_SetRenderDrawColor(instance->renderer, 0xFF, 0xFF, 0xFF, 0xFF);
-	SDL_RenderDrawLine(instance->renderer, 10, 10, 100, 100);
+	int i, j;
+
+	for (i = x; i <= (x + width); i++)
+		for (j = y; j <= (y + height); j++)
+			drawPixel(i, j, color);
 }
 
 /**
- * draw_image - A function to draw an image onto the SDL Screen Surface
+ * drawLine - draw a line
  *
- * @instance: An SDL instance of type struct SDL_Instance
- */
-void draw_image(SDL_Instance *instance)
-{
-	/* Apply the image */
-	SDL_BlitSurface(instance->image, NULL, instance->screenSurface, NULL);
-	/* Update the surface */
-	SDL_UpdateWindowSurface(instance->window);
-}
-
-/**
- * drawMiniMap - A function to draw the minimap.
+ * @x0: x coordinate init
+ * @y0: y coordinate init
+ * @x1: x coordinate end
+ * @y1: y coordinate end
+ * @color: pixel color
  *
- * @WorldMap: The map to draw
- * @instance: The SDL_Instance to draw to
- * @player: The player to draw
  */
-void drawMiniMap(int (*WorldMap)[MAP_WIDTH], SDL_Instance *instance,
-				Vector player)
-{
-	SDL_Rect curRect;
-	ColorRGBA color;
 
-	for (int x = 0; x < MAP_WIDTH; x++)
+void drawLine(int x0, int y0, int x1, int y1, color_t color)
+{
+	float xIncrement, yIncrement, currentX, currentY;
+	int i, longestSideLength, deltaX,  deltaY;
+
+	deltaX = (x1 - x0);
+	deltaY = (y1 - y0);
+
+	longestSideLength = (abs(deltaX) >= abs(deltaY)) ? abs(deltaX)
+					: abs(deltaY);
+
+	xIncrement = deltaX / (float)longestSideLength;
+	yIncrement = deltaY / (float)longestSideLength;
+
+	currentX = x0;
+	currentY = y0;
+
+	for (i = 0; i < longestSideLength; i++)
 	{
-		for (int y = 0; y < MAP_HEIGHT; y++)
-		{
-			SDL_Rect rect = {x * TILE_SIZE, y * TILE_SIZE, TILE_SIZE, TILE_SIZE};
-
-			color_walls(WorldMap, x, y, &color, 0);
-			SDL_SetRenderDrawColor(instance->renderer, color.red, color.green,
-									color.blue, (Uint8) (255 * color.alpha));
-			SDL_RenderFillRect(instance->renderer, &rect);
-		}
+		drawPixel(round(currentX), round(currentY), color);
+		currentX += xIncrement;
+		currentY += yIncrement;
 	}
-
-	/* Draw player */
-	curRect.x = player.posX * TILE_SIZE;
-	curRect.y = player.posY * TILE_SIZE;
-	curRect.w = TILE_SIZE;
-	curRect.h = TILE_SIZE;
-	color = rgba_color_code("indigo");
-	SDL_SetRenderDrawColor(instance->renderer, color.red, color.green,
-							color.blue, (Uint8) (255 * color.alpha));
-	SDL_RenderFillRect(instance->renderer, &curRect);
-
-	/* Update screen */
-	SDL_RenderPresent(instance->renderer);
 }
--- src/raycasting.c
+++ src/raycasting.c
@@ -1,201 +1,185 @@
-#include "../inc/main.h"
+#include "../inc/header.h"
+
+ray_t rays[NUM_RAYS];
+
+static bool foundHorzWallHit, foundVertWallHit;
+static float horzWallHitX, horzWallHitY, vertWallHitX, vertWallHitY;
+static int horzWallContent, vertWallContent;
+
 
 /**
- * raycaster - A function to perform raycasting operations.
+ * horzIntersection - Finds horizontal intersection with the wall
+ * @rayAngle: current ray angle
  *
- * @object: struct of type Vector containing all coordinates of that oject
- * @time: Time of current frame
- * @oldTime: Time of previous frame
- * @instance: struct of type SDL_Instance
- * @event: An SDL Event
- * @delay: The delay flag (true or false)
- * @keys: A boolean array to store key states
- * Return: 0 (Success)
  */
-int raycaster(Vector object, double *time, double *oldTime,
-			SDL_Instance *instance, SDL_Event *event, bool delay,
-			const unsigned char *keys)
+
+void horzIntersection(float rayAngle)
 {
-	while (!done(event, delay, keys))
-		break;
-	for (int x = 0; x < SCREEN_WIDTH; x++)
-	{
-		/* x-coord in camera space */
-		double cameraX = 2 * x / SCREEN_WIDTH - 1;
-		/* Calculate ray direction vector */
-		double rayDirX = object.dirX + object.planeX * cameraX;
-		double rayDirY = object.dirY + object.planeY * cameraX;
-		/* Calculate map position of ray origin */
-		int mapX = floor(object.posX);
-		int mapY = floor(object.posY);
-		/* Calculate distance to next x and y grid lines */
-		double deltaDistX = (rayDirX == 0) ? 1e30 : fabs(1 / rayDirX);
-		double deltaDistY = (rayDirX == 0) ? 1e30 : fabs(1 / rayDirY);
-		/* Calculate initial step and side distances */
-		double sideDistX, sideDistY, perpWallDist;
-		int stepX, stepY, wall_height, wall_top, wall_bottom;
-		int hit = 0;
-		int side = 0; /* was a NS or a EW wall hit? */
-		int worldMap[MAP_WIDTH][MAP_HEIGHT];
-		/* World map */
-		generate_map(worldMap);
-		calculate_distances(object, rayDirX, rayDirY, &sideDistX,
-			&sideDistY, &stepX, &stepY, mapX, mapY, deltaDistX, deltaDistY);
-		int wall_code = DDA(&hit, &side, &sideDistX, &sideDistY, deltaDistX,
-							deltaDistY, &mapX, &mapY, stepX, stepY, worldMap);
+	float nextHorzTouchX, nextHorzTouchY, xintercept, yintercept, xstep, ystep;
 
-		calcuate_projection(side, sideDistX, sideDistY,	deltaDistX, deltaDistY,
-						&perpWallDist, &wall_height, &wall_top, &wall_bottom);
-		render_walls(instance->renderer, wall_top, wall_bottom, wall_code, side, x);
-		/* drawMiniMap(worldMap, instance, object); */
-	}
-	fps_count(time, oldTime);
+	foundHorzWallHit = false;
+	horzWallHitX = horzWallHitY = horzWallContent = 0;
 
-	return (0);
-}
+	yintercept = floor(player.y / TILE_SIZE) * TILE_SIZE;
+	yintercept += isRayFacingDown(rayAngle) ? TILE_SIZE : 0;
 
-/**
- * calculate_distances - A function to calculate all important coordinates at
- * a given point in time.
- *
- * @object: struct of type Vector containing all coordinates of that oject
- * @rayDirX: Ray position and direction in x-axis
- * @rayDirY: Ray position and direction in y-axis
- * @sideDistX: Length of ray from current position to next position on x-axis
- * @sideDistY: Length of ray from current position to next position on y-axis
- * @stepX: What direction to step in x-axis (either +1 or -1)
- * @stepY: What direction to step in y-axis (either +1 or -1)
- * @mapX: x-coordinate of current box of the map we're in
- * @mapY: y-coordinate of current box of the map we're in
- * @deltaDistX: Length of ray from one point to another on x-axis
- * @deltaDistY: Length of ray from one point to another on y-axis
- */
-void calculate_distances(Vector object, double rayDirX, double rayDirY,
-						double *sideDistX, double *sideDistY, int *stepX,
-						int *stepY, int mapX, int mapY, double deltaDistX,
-						double deltaDistY)
-{
-	*stepX = 0;
-	*stepY = 0;
-	/* calculate step and initial sideDist */
-	if (rayDirX < 0)
-	{
-		*stepX = -1;
-		*sideDistX = (object.posX - mapX) * deltaDistX;
-	} else
-	{
-		*stepX = 1;
-		*sideDistX = (mapX + 1.0 - object.posX) * deltaDistX;
-	}
+	xintercept = player.x + (yintercept - player.y) / tan(rayAngle);
 
-	if (rayDirY < 0)
-	{
-		*stepY = -1;
-		*sideDistY = (object.posY - mapY) * deltaDistY;
-	} else
-	{
-		*stepY = 1;
-		*sideDistY = (mapY + 1.0 - object.posY) * deltaDistY;
-	}
-}
+	ystep = TILE_SIZE;
+	ystep *= isRayFacingUp(rayAngle) ? -1 : 1;
+	xstep = TILE_SIZE / tan(rayAngle);
+	xstep *= (isRayFacingLeft(rayAngle) && xstep > 0) ? -1 : 1;
+	xstep *= (isRayFacingRight(rayAngle) && xstep < 0) ? -1 : 1;
+	nextHorzTouchX = xintercept;
+	nextHorzTouchY = yintercept;
 
-/**
- * DDA - Digital Differential Analysis
- *
- * @hit: Integer value (1 if wall was hit else 0)
- * @side: Side of the wall that was hit (NS or EW)
- * @sideDistX: Length of ray from current position to next position on x-axis
- * @sideDistY: Length of ray from current position to next position on y-axis
- * @deltaDistX: Length of ray from one point to another on x-axis
- * @deltaDistY: Length of ray from one point to another on y-axis
- * @mapX: x-coordinate of current box of the map we're in
- * @mapY: y-coordinate of current box of the map we're in
- * @stepX: What direction to step in x-axis (either +1 or -1)
- * @stepY: What direction to step in y-axis (either +1 or -1)
- * @worldMap: A 2-dimensional array of integer values
- * Return: Code of the wall if wall was hit else 0
- */
-int DDA(int *hit, int *side, double *sideDistX, double *sideDistY,
-		double deltaDistX, double deltaDistY, int *mapX, int *mapY, int stepX,
-		int stepY, int (*worldMap)[MAP_WIDTH])
-{
-	/* perform DDA */
-	while (*hit == 0)
+	while (isInsideMap(nextHorzTouchX, nextHorzTouchY))
 	{
-		/* Move to next grid cell */
-		if (*sideDistX < *sideDistY)
+		float xToCheck = nextHorzTouchX;
+		float yToCheck = nextHorzTouchY + (isRayFacingUp(rayAngle) ? -1 : 0);
+
+		if (DetectCollision(xToCheck, yToCheck))
 		{
-			*sideDistX += deltaDistX;
-			*mapX += stepX;
-			*hit = worldMap[*mapX][*mapY];
-			*side = 0;
-		} else
-		{
-			*sideDistY += deltaDistY;
-			*mapY += stepY;
-			*hit = worldMap[*mapX][*mapY];
-			*side = 1;
+			horzWallHitX = nextHorzTouchX;
+			horzWallHitY = nextHorzTouchY;
+			horzWallContent = getMapValue((int)floor(yToCheck / TILE_SIZE),
+						 (int)floor(xToCheck / TILE_SIZE));
+																								foundHorzWallHit = true;
+			break;
 		}
-	}
 
-	return (*hit);
+		nextHorzTouchX += xstep;
+		nextHorzTouchY += ystep;
+	}
 }
 
 /**
- * calculate_projection - A function that calculates the values necessary for
- * camera projection.
+ * vertIntersection - Finds vertical intersection with the wall
+ * @rayAngle: current ray angle
  *
- * @side: Side of the wall that was hit (NS or EW)
- * @sideDistX: Length of ray from current position to next position on x-axis
- * @sideDistY: Length of ray from current position to next position on y-axis
- * @deltaDistX: Length of ray from one point to another on x-axis
- * @deltaDistY: Length of ray from one point to another on y-axis
- * @perpWallDist: Perpendicular distance from ray to wall
- * @wall_height: Height of the wall
- * @wall_top: Top of the wall
- * @wall_bottom: Bottom of the wall
- * Return: struct of type Projection
  */
-Projection calcuate_projection(int side, double sideDistX, double sideDistY,
-						double deltaDistX, double deltaDistY,
-						double *perpWallDist, int *wall_height, int *wall_top,
-						int *wall_bottom)
+void vertIntersection(float rayAngle)
 {
-	Projection pixel_coordinates;
+	float nextVertTouchX, nextVertTouchY;
+	float xintercept, yintercept, xstep, ystep;
 
-	/*
-	* Calculate distance projected on camera direction
-	* (Euclidean distance would give fisheye effect!)
-	*/
-	if (side == 0)
-		*perpWallDist = (sideDistX - deltaDistX);
+	foundVertWallHit = false;
+	vertWallHitX = 0;
+	vertWallHitY = 0;
+	vertWallContent = 0;
+
+	xintercept = floor(player.x / TILE_SIZE) * TILE_SIZE;
+	xintercept += isRayFacingRight(rayAngle) ? TILE_SIZE : 0;
+	yintercept = player.y + (xintercept - player.x) * tan(rayAngle);
+
+	xstep = TILE_SIZE;
+	xstep *= isRayFacingLeft(rayAngle) ? -1 : 1;
+	ystep = TILE_SIZE * tan(rayAngle);
+	ystep *= (isRayFacingUp(rayAngle) && ystep > 0) ? -1 : 1;
+	ystep *= (isRayFacingDown(rayAngle) && ystep < 0) ? -1 : 1;
+	nextVertTouchX = xintercept;
+	nextVertTouchY = yintercept;
+
+	while (isInsideMap(nextVertTouchX, nextVertTouchY))
+	{
+		float xToCheck = nextVertTouchX + (isRayFacingLeft(rayAngle) ? -1 : 0);
+		float yToCheck = nextVertTouchY;
+
+		if (DetectCollision(xToCheck, yToCheck))
+		{
+			vertWallHitX = nextVertTouchX;
+			vertWallHitY = nextVertTouchY;
+			vertWallContent = getMapValue((int)floor(yToCheck / TILE_SIZE),
+					 (int)floor(xToCheck / TILE_SIZE));
+			foundVertWallHit = true;
+			break;
+		}
+		nextVertTouchX += xstep;
+		nextVertTouchY += ystep;
+	}
+}
+
+/**
+ * castRay - casting of each ray
+ *
+ * @rayAngle: current ray angle
+ * @stripId: ray strip identifier
+ *
+ */
+
+void castRay(float rayAngle, int stripId)
+{
+	float horzHitDistance, vertHitDistance;
+
+	rayAngle = remainder(rayAngle, TWO_PI);
+	if (rayAngle < 0)
+		rayAngle = TWO_PI + rayAngle;
+
+	horzIntersection(rayAngle);
+
+	vertIntersection(rayAngle);
+
+	horzHitDistance = foundHorzWallHit
+		? distanceBetweenPoints(player.x, player.y, horzWallHitX,
+				horzWallHitY) : FLT_MAX;
+	vertHitDistance = foundVertWallHit
+		? distanceBetweenPoints(player.x, player.y, vertWallHitX,
+				vertWallHitY) : FLT_MAX;
+
+	if (vertHitDistance < horzHitDistance)
+	{
+		rays[stripId].distance = vertHitDistance;
+		rays[stripId].wallHitX = vertWallHitX;
+		rays[stripId].wallHitY = vertWallHitY;
+		rays[stripId].wallHitContent = vertWallContent;
+		rays[stripId].wasHitVertical = true;
+		rays[stripId].rayAngle = rayAngle;
+	}
 	else
-		*perpWallDist = (sideDistY - deltaDistY);
+	{
+		rays[stripId].distance = horzHitDistance;
+		rays[stripId].wallHitX = horzWallHitX;
+		rays[stripId].wallHitY = horzWallHitY;
+		rays[stripId].wallHitContent = horzWallContent;
+		rays[stripId].wasHitVertical = false;
+		rays[stripId].rayAngle = rayAngle;
+	}
+}
 
-	/* Calculate height of line to draw on screen */
-	int lineHeight = (int)(SCREEN_HEIGHT / *perpWallDist);
+/**
+ * castAllRays - cast of all rays
+ *
+ */
 
-	/* calculate lowest and highest pixel to fill in current stripe */
-	int drawStart = -lineHeight / 2 + SCREEN_HEIGHT / 2;
-	int drawEnd = lineHeight / 2 + SCREEN_HEIGHT / 2;
+void castAllRays(void)
+{
+	int col;
 
-	if (drawStart < 0)
-		drawStart = 0;
-	if (drawEnd >= SCREEN_HEIGHT)
-		drawEnd = SCREEN_HEIGHT - 1;
+	for (col = 0; col < NUM_RAYS; col++)
+	{
+		float rayAngle = player.rotationAngle +
+			atan((col - NUM_RAYS / 2) / PROJ_PLANE);
+		castRay(rayAngle, col);
+	}
+}
 
-	pixel_coordinates.drawStart = drawStart;
-	pixel_coordinates.drawEnd = drawEnd;
+/**
+ * renderRays - draw all the rays
+ *
+ */
 
-	/* Calculate wall height on screen */
-	*wall_height = (int)(SCREEN_HEIGHT / *perpWallDist);
+void renderRays(void)
+{
+	int i;
 
-	/* Calculate top and bottom pixels of wall on screen */
-	*wall_top = SCREEN_HEIGHT / 2 - *wall_height / 2;
-	*wall_bottom = *wall_top + *wall_height;
-
-	if (*wall_top < 0 && *wall_bottom)
-		*wall_top = 0;
-
-	return (pixel_coordinates);
+	for (i = 0; i < NUM_RAYS; i += 50)
+	{
+		drawLine(
+				player.x * MINIMAP_SCALE_FACTOR,
+				player.y * MINIMAP_SCALE_FACTOR,
+				rays[i].wallHitX * MINIMAP_SCALE_FACTOR,
+				rays[i].wallHitY * MINIMAP_SCALE_FACTOR,
+				0xFF0000FF
+			);
+	}
 }
